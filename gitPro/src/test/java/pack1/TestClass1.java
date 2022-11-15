a528efa5af83235c45fb65c770fc508b6983a1ce
SygYeole7498 <107597358+SygYeole7498@users.noreply.github.com>
2022-11-15 09:03:30 +0530

    Update TestClass1.java

--- gitPro/src/test/java/pack1/TestClass1.java
+++ gitPro/src/test/java/pack1/TestClass1.java
@@ -3,12 +3,5 @@ package pack1;
 public class TestClass1 {
 public static void main(String[] args) {
 	System.out.println("TC01");
-	System.out.println("TC01");
-	System.out.println("TC01");
-	System.out.println("TC01");
-	System.out.println("TC01");
-	System.out.println("TC01");
-	System.out.println("TC01");
-	System.out.println("TC01");
 }
 }
