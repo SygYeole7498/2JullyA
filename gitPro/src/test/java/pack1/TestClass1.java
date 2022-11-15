dadb11f06b34df524c9bddc63e34cf314b440f7a
SUYOG YEOLE <SUYOG YEOLE@DESKTOP-C59MQFQ>
2022-11-15 08:53:19 +0530

    First Commit

---
+++ gitPro/src/test/java/pack1/TestClass1.java
@@ -0,0 +1,14 @@
+package pack1;
+
+public class TestClass1 {
+public static void main(String[] args) {
+	System.out.println("TC01");
+	System.out.println("TC01");
+	System.out.println("TC01");
+	System.out.println("TC01");
+	System.out.println("TC01");
+	System.out.println("TC01");
+	System.out.println("TC01");
+	System.out.println("TC01");
+}
+}
